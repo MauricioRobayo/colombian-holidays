45f074f9a1f7cf3950d4f6ee303478f65aec82cb
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-11-17 14:35:58 -0500

    build: run tests on lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   linters: {
-    "src/**/*.js": ["eslint --fix", "git add"],
+    "src/**/*.js": [
+      "eslint --fix",
+      "git add",
+      "jest --bail --findRelatedTests"
+    ],
     "**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}": [
       "prettier --write",
       "git add"
