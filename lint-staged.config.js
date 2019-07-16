06e9677f5d81e0e4da9d1c1efee56c9fbda3e021
Mauricio Robayo <rfmajo@gmail.com>
2019-07-16 18:16:48 -0500

    chore: update lint-staged config file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  linters: {
-    "src/**/*.js": [
-      "eslint --fix",
-      "git add",
-      "jest --bail --findRelatedTests"
-    ],
-    "**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}": [
-      "prettier --write",
-      "git add"
-    ]
-  }
+  "src/**/*.js": ["eslint --fix", "git add", "jest --bail --findRelatedTests"],
+  "**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}": [
+    "prettier --write",
+    "git add"
+  ]
 };
