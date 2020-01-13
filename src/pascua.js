d0471d9f491278dcf647f52114c81a3d7f9a95a2
Mauricio Robayo <me@mauriciorobayo.com>
2020-01-13 17:35:18 -0500

    style: update format according to prettier

--- src/pascua.js
+++ src/pascua.js
@@ -1,45 +1,45 @@
 const holidays = [
-  { type: 1, name: "Año Nuevo", day: 1, month: 1 },
-  { type: 1, name: "Día del Trabajo", day: 1, month: 5 },
-  { type: 1, name: "Grito de la Independencia", day: 20, month: 7 },
-  { type: 1, name: "Batalla de Boyacá", day: 7, month: 8 },
-  { type: 1, name: "Inmaculada Concepción", day: 8, month: 12 },
-  { type: 1, name: "Navidad", day: 25, month: 12 },
-  { type: 2, name: "Reyes Magos", day: 6, month: 1 },
-  { type: 2, name: "San José", day: 19, month: 3 },
-  { type: 2, name: "San Pedro y San Pablo", day: 29, month: 6 },
-  { type: 2, name: "Asunción de la Virgen", day: 15, month: 8 },
-  { type: 2, name: "Día de la Raza", day: 12, month: 10 },
-  { type: 2, name: "Todos los Santos", day: 1, month: 11 },
-  { type: 2, name: "Independencia de Cartagena", day: 11, month: 11 },
-  { type: 3, name: "Jueves Santo", offset: -3 },
-  { type: 3, name: "Viernes Santo", offset: -2 },
-  { type: 3, name: "Ascensión de Jesús", offset: 43 },
-  { type: 3, name: "Corpus Christi", offset: 64 },
-  { type: 3, name: "Sagrado Corazón de Jesús", offset: 71 }
-];
+  { type: 1, name: 'Año Nuevo', day: 1, month: 1 },
+  { type: 1, name: 'Día del Trabajo', day: 1, month: 5 },
+  { type: 1, name: 'Grito de la Independencia', day: 20, month: 7 },
+  { type: 1, name: 'Batalla de Boyacá', day: 7, month: 8 },
+  { type: 1, name: 'Inmaculada Concepción', day: 8, month: 12 },
+  { type: 1, name: 'Navidad', day: 25, month: 12 },
+  { type: 2, name: 'Reyes Magos', day: 6, month: 1 },
+  { type: 2, name: 'San José', day: 19, month: 3 },
+  { type: 2, name: 'San Pedro y San Pablo', day: 29, month: 6 },
+  { type: 2, name: 'Asunción de la Virgen', day: 15, month: 8 },
+  { type: 2, name: 'Día de la Raza', day: 12, month: 10 },
+  { type: 2, name: 'Todos los Santos', day: 1, month: 11 },
+  { type: 2, name: 'Independencia de Cartagena', day: 11, month: 11 },
+  { type: 3, name: 'Jueves Santo', offset: -3 },
+  { type: 3, name: 'Viernes Santo', offset: -2 },
+  { type: 3, name: 'Ascensión de Jesús', offset: 43 },
+  { type: 3, name: 'Corpus Christi', offset: 64 },
+  { type: 3, name: 'Sagrado Corazón de Jesús', offset: 71 },
+]
 
 function validateYear(year) {
-  const int = Number.parseInt(year, 10);
+  const int = Number.parseInt(year, 10)
   // En 1983 se expide la ley por la cual se corren algunos festivos al próximo lunes
   // http://www.alcaldiabogota.gov.co/sisjur/normas/Norma1.jsp?i=4954
   if (Number.isNaN(int) || int < 1984) {
-    throw new Error("Invalid year. Should be an integer > 1983");
+    throw new Error('Invalid year. Should be an integer > 1983')
   }
-  return int;
+  return int
 }
 
 function isValidDate(date) {
   return (
-    Object.prototype.toString.call(date) === "[object Date]" &&
+    Object.prototype.toString.call(date) === '[object Date]' &&
     validateYear(date.getFullYear())
-  );
+  )
 }
 
 function addDays(date, amount) {
-  const resultDate = new Date(date.getTime());
-  resultDate.setDate(resultDate.getDate() + amount);
-  return resultDate;
+  const resultDate = new Date(date.getTime())
+  resultDate.setDate(resultDate.getDate() + amount)
+  return resultDate
 }
 
 function isSameDate(date1, date2) {
@@ -47,13 +47,13 @@ function isSameDate(date1, date2) {
     // No need to check year as we ceate the holiday
     // using the year from the given date to check
     date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth()
-  );
+  )
 }
 
 function getNextDayOfWeek(date, dayOfWeek) {
-  const resultDate = new Date(date.getTime());
-  resultDate.setDate(date.getDate() + ((7 + dayOfWeek - date.getDay()) % 7));
-  return resultDate;
+  const resultDate = new Date(date.getTime())
+  resultDate.setDate(date.getDate() + ((7 + dayOfWeek - date.getDay()) % 7))
+  return resultDate
 }
 
 /**
@@ -68,79 +68,79 @@ function getNextDayOfWeek(date, dayOfWeek) {
  *   Pascua + 71 =  "Sagrado Corazón de Jesús"
  */
 function getPascua(year) {
-  const A = year % 19;
-  const B = Math.floor(year / 100);
-  const C = year % 100;
-  const D = Math.floor(B / 4);
-  const E = B % 4;
-  const F = Math.floor((B + 8) / 25);
-  const G = Math.floor((B - F + 1) / 3);
-  const H = (19 * A + B - D - G + 15) % 30;
-  const I = Math.floor(C / 4);
-  const K = C % 4;
-  const L = (32 + 2 * E + 2 * I - H - K) % 7;
-  const M = Math.floor((A + 11 * H + 22 * L) / 451);
-  const N = H + L - 7 * M + 114;
-  const month = Math.floor(N / 31);
-  const day = 1 + (N % 31);
-  const pascua = new Date(year, month - 1, day);
-  return pascua;
+  const A = year % 19
+  const B = Math.floor(year / 100)
+  const C = year % 100
+  const D = Math.floor(B / 4)
+  const E = B % 4
+  const F = Math.floor((B + 8) / 25)
+  const G = Math.floor((B - F + 1) / 3)
+  const H = (19 * A + B - D - G + 15) % 30
+  const I = Math.floor(C / 4)
+  const K = C % 4
+  const L = (32 + 2 * E + 2 * I - H - K) % 7
+  const M = Math.floor((A + 11 * H + 22 * L) / 451)
+  const N = H + L - 7 * M + 114
+  const month = Math.floor(N / 31)
+  const day = 1 + (N % 31)
+  const pascua = new Date(year, month - 1, day)
+  return pascua
 }
 
 function getHoliday(date = new Date()) {
   if (!isValidDate(date)) {
-    throw new Error("Invalid date.");
+    throw new Error('Invalid date.')
   }
-  const year = date.getFullYear();
+  const year = date.getFullYear()
   for (let i = 0; i < holidays.length; i += 1) {
     if (holidays[i].type === 1) {
-      const holiday = new Date(year, holidays[i].month - 1, holidays[i].day);
+      const holiday = new Date(year, holidays[i].month - 1, holidays[i].day)
       if (isSameDate(date, holiday)) {
-        return holidays[i].name;
+        return holidays[i].name
       }
     }
     if (holidays[i].type === 2) {
-      const holiday = new Date(year, holidays[i].month - 1, holidays[i].day);
-      const nextMonday = getNextDayOfWeek(holiday, 1);
+      const holiday = new Date(year, holidays[i].month - 1, holidays[i].day)
+      const nextMonday = getNextDayOfWeek(holiday, 1)
       if (isSameDate(date, nextMonday)) {
-        return holidays[i].name;
+        return holidays[i].name
       }
     }
     if (holidays[i].type === 3) {
-      const pascua = getPascua(year);
-      const pascuaHoliday = addDays(pascua, holidays[i].offset);
+      const pascua = getPascua(year)
+      const pascuaHoliday = addDays(pascua, holidays[i].offset)
       if (isSameDate(date, pascuaHoliday)) {
-        return holidays[i].name;
+        return holidays[i].name
       }
     }
   }
-  return "";
+  return ''
 }
 
 function getAllHolidays(year = new Date().getFullYear()) {
-  const validYear = validateYear(year);
-  const yearHolidays = [];
+  const validYear = validateYear(year)
+  const yearHolidays = []
   for (let i = 0; i < holidays.length; i += 1) {
-    let holidayDate;
+    let holidayDate
     if (holidays[i].type === 1) {
-      holidayDate = new Date(validYear, holidays[i].month - 1, holidays[i].day);
+      holidayDate = new Date(validYear, holidays[i].month - 1, holidays[i].day)
     }
     if (holidays[i].type === 2) {
       holidayDate = getNextDayOfWeek(
         new Date(validYear, holidays[i].month - 1, holidays[i].day),
-        1
-      );
+        1,
+      )
     }
     if (holidays[i].type === 3) {
-      holidayDate = addDays(getPascua(validYear), holidays[i].offset);
+      holidayDate = addDays(getPascua(validYear), holidays[i].offset)
     }
     yearHolidays.push({
       date: holidayDate.toISOString().substring(0, 10),
       type: holidays[i].type,
-      name: holidays[i].name
-    });
+      name: holidays[i].name,
+    })
   }
-  return yearHolidays;
+  return yearHolidays
 }
 
-module.exports = { getHoliday, getAllHolidays };
+module.exports = { getHoliday, getAllHolidays }
--- src/pascua.test.js
+++ src/pascua.test.js
@@ -1,172 +1,172 @@
-const { getAllHolidays, getHoliday } = require("./pascua");
+const { getAllHolidays, getHoliday } = require('./pascua')
 
-const times = ["00:00", "12:00", "23:59"];
+const times = ['00:00', '12:00', '23:59']
 
-describe("get the name of a given holiday date", () => {
+describe('get the name of a given holiday date', () => {
   const holidayCases = [
-    { date: "2017/04/12", name: "" },
-    { date: "2017/04/13", name: "Jueves Santo" },
-    { date: "2014/04/18", name: "Viernes Santo" },
-    { date: "2017/05/29", name: "Ascensión de Jesús" },
-    { date: "2010/05/17", name: "Ascensión de Jesús" },
-    { date: "2011/06/27", name: "Corpus Christi" },
-    { date: "2016/06/06", name: "Sagrado Corazón de Jesús" },
-    { date: "2017/04/15", name: "" },
-    { date: "1986/01/01", name: "Año Nuevo" },
-    { date: "1984/12/31", name: "" },
-    { date: "2013/01/07", name: "Reyes Magos" },
-    { date: "2018/01/08", name: "Reyes Magos" },
-    { date: "2025/12/25", name: "Navidad" },
-    { date: "1987/12/24", name: "" },
-    { date: "2017/03/13", name: "" },
-    { date: "2017/01/09", name: "Reyes Magos" },
-    { date: "2014/01/06", name: "Reyes Magos" },
-    { date: "2011/08/15", name: "Asunción de la Virgen" },
-    { date: "2013/11/11", name: "Independencia de Cartagena" },
-    { date: "2018/11/05", name: "Todos los Santos" },
-    { date: "2013/10/14", name: "Día de la Raza" },
-    { date: "2015/10/12", name: "Día de la Raza" },
-    { date: "2010/10/18", name: "Día de la Raza" }
-  ];
+    { date: '2017/04/12', name: '' },
+    { date: '2017/04/13', name: 'Jueves Santo' },
+    { date: '2014/04/18', name: 'Viernes Santo' },
+    { date: '2017/05/29', name: 'Ascensión de Jesús' },
+    { date: '2010/05/17', name: 'Ascensión de Jesús' },
+    { date: '2011/06/27', name: 'Corpus Christi' },
+    { date: '2016/06/06', name: 'Sagrado Corazón de Jesús' },
+    { date: '2017/04/15', name: '' },
+    { date: '1986/01/01', name: 'Año Nuevo' },
+    { date: '1984/12/31', name: '' },
+    { date: '2013/01/07', name: 'Reyes Magos' },
+    { date: '2018/01/08', name: 'Reyes Magos' },
+    { date: '2025/12/25', name: 'Navidad' },
+    { date: '1987/12/24', name: '' },
+    { date: '2017/03/13', name: '' },
+    { date: '2017/01/09', name: 'Reyes Magos' },
+    { date: '2014/01/06', name: 'Reyes Magos' },
+    { date: '2011/08/15', name: 'Asunción de la Virgen' },
+    { date: '2013/11/11', name: 'Independencia de Cartagena' },
+    { date: '2018/11/05', name: 'Todos los Santos' },
+    { date: '2013/10/14', name: 'Día de la Raza' },
+    { date: '2015/10/12', name: 'Día de la Raza' },
+    { date: '2010/10/18', name: 'Día de la Raza' },
+  ]
   holidayCases.forEach(holiday => {
     times.forEach(time => {
-      const datetime = `${holiday.date} ${time}`;
+      const datetime = `${holiday.date} ${time}`
       it(`should return '${holiday.name}' for ${datetime}`, () => {
-        const date = new Date(datetime);
-        expect(getHoliday(date)).toBe(holiday.name);
-      });
-    });
+        const date = new Date(datetime)
+        expect(getHoliday(date)).toBe(holiday.name)
+      })
+    })
     it(`should return the info for the current day when no date given`, () => {
-      const currDate = new Date();
-      expect(getHoliday()).toBe(getHoliday(currDate));
-    });
-  });
-});
+      const currDate = new Date()
+      expect(getHoliday()).toBe(getHoliday(currDate))
+    })
+  })
+})
 
-describe("Gets all holidays for a given year", () => {
+describe('Gets all holidays for a given year', () => {
   const holidaysYears = {
     2010: [
-      { date: "2010-01-01", type: 1, name: "Año Nuevo" },
-      { date: "2010-05-01", type: 1, name: "Día del Trabajo" },
-      { date: "2010-07-20", type: 1, name: "Grito de la Independencia" },
-      { date: "2010-08-07", type: 1, name: "Batalla de Boyacá" },
-      { date: "2010-12-08", type: 1, name: "Inmaculada Concepción" },
-      { date: "2010-12-25", type: 1, name: "Navidad" },
-      { date: "2010-01-11", type: 2, name: "Reyes Magos" },
-      { date: "2010-03-22", type: 2, name: "San José" },
-      { date: "2010-07-05", type: 2, name: "San Pedro y San Pablo" },
-      { date: "2010-08-16", type: 2, name: "Asunción de la Virgen" },
-      { date: "2010-10-18", type: 2, name: "Día de la Raza" },
-      { date: "2010-11-01", type: 2, name: "Todos los Santos" },
-      { date: "2010-11-15", type: 2, name: "Independencia de Cartagena" },
-      { date: "2010-04-01", type: 3, name: "Jueves Santo" },
-      { date: "2010-04-02", type: 3, name: "Viernes Santo" },
-      { date: "2010-05-17", type: 3, name: "Ascensión de Jesús" },
-      { date: "2010-06-07", type: 3, name: "Corpus Christi" },
-      { date: "2010-06-14", type: 3, name: "Sagrado Corazón de Jesús" }
+      { date: '2010-01-01', type: 1, name: 'Año Nuevo' },
+      { date: '2010-05-01', type: 1, name: 'Día del Trabajo' },
+      { date: '2010-07-20', type: 1, name: 'Grito de la Independencia' },
+      { date: '2010-08-07', type: 1, name: 'Batalla de Boyacá' },
+      { date: '2010-12-08', type: 1, name: 'Inmaculada Concepción' },
+      { date: '2010-12-25', type: 1, name: 'Navidad' },
+      { date: '2010-01-11', type: 2, name: 'Reyes Magos' },
+      { date: '2010-03-22', type: 2, name: 'San José' },
+      { date: '2010-07-05', type: 2, name: 'San Pedro y San Pablo' },
+      { date: '2010-08-16', type: 2, name: 'Asunción de la Virgen' },
+      { date: '2010-10-18', type: 2, name: 'Día de la Raza' },
+      { date: '2010-11-01', type: 2, name: 'Todos los Santos' },
+      { date: '2010-11-15', type: 2, name: 'Independencia de Cartagena' },
+      { date: '2010-04-01', type: 3, name: 'Jueves Santo' },
+      { date: '2010-04-02', type: 3, name: 'Viernes Santo' },
+      { date: '2010-05-17', type: 3, name: 'Ascensión de Jesús' },
+      { date: '2010-06-07', type: 3, name: 'Corpus Christi' },
+      { date: '2010-06-14', type: 3, name: 'Sagrado Corazón de Jesús' },
     ],
     2012: [
-      { date: "2012-01-01", type: 1, name: "Año Nuevo" },
-      { date: "2012-05-01", type: 1, name: "Día del Trabajo" },
-      { date: "2012-07-20", type: 1, name: "Grito de la Independencia" },
-      { date: "2012-08-07", type: 1, name: "Batalla de Boyacá" },
-      { date: "2012-12-08", type: 1, name: "Inmaculada Concepción" },
-      { date: "2012-12-25", type: 1, name: "Navidad" },
-      { date: "2012-01-09", type: 2, name: "Reyes Magos" },
-      { date: "2012-03-19", type: 2, name: "San José" },
-      { date: "2012-07-02", type: 2, name: "San Pedro y San Pablo" },
-      { date: "2012-08-20", type: 2, name: "Asunción de la Virgen" },
-      { date: "2012-10-15", type: 2, name: "Día de la Raza" },
-      { date: "2012-11-05", type: 2, name: "Todos los Santos" },
-      { date: "2012-11-12", type: 2, name: "Independencia de Cartagena" },
-      { date: "2012-04-05", type: 3, name: "Jueves Santo" },
-      { date: "2012-04-06", type: 3, name: "Viernes Santo" },
-      { date: "2012-05-21", type: 3, name: "Ascensión de Jesús" },
-      { date: "2012-06-11", type: 3, name: "Corpus Christi" },
-      { date: "2012-06-18", type: 3, name: "Sagrado Corazón de Jesús" }
+      { date: '2012-01-01', type: 1, name: 'Año Nuevo' },
+      { date: '2012-05-01', type: 1, name: 'Día del Trabajo' },
+      { date: '2012-07-20', type: 1, name: 'Grito de la Independencia' },
+      { date: '2012-08-07', type: 1, name: 'Batalla de Boyacá' },
+      { date: '2012-12-08', type: 1, name: 'Inmaculada Concepción' },
+      { date: '2012-12-25', type: 1, name: 'Navidad' },
+      { date: '2012-01-09', type: 2, name: 'Reyes Magos' },
+      { date: '2012-03-19', type: 2, name: 'San José' },
+      { date: '2012-07-02', type: 2, name: 'San Pedro y San Pablo' },
+      { date: '2012-08-20', type: 2, name: 'Asunción de la Virgen' },
+      { date: '2012-10-15', type: 2, name: 'Día de la Raza' },
+      { date: '2012-11-05', type: 2, name: 'Todos los Santos' },
+      { date: '2012-11-12', type: 2, name: 'Independencia de Cartagena' },
+      { date: '2012-04-05', type: 3, name: 'Jueves Santo' },
+      { date: '2012-04-06', type: 3, name: 'Viernes Santo' },
+      { date: '2012-05-21', type: 3, name: 'Ascensión de Jesús' },
+      { date: '2012-06-11', type: 3, name: 'Corpus Christi' },
+      { date: '2012-06-18', type: 3, name: 'Sagrado Corazón de Jesús' },
     ],
     2015: [
-      { date: "2015-01-01", type: 1, name: "Año Nuevo" },
-      { date: "2015-05-01", type: 1, name: "Día del Trabajo" },
-      { date: "2015-07-20", type: 1, name: "Grito de la Independencia" },
-      { date: "2015-08-07", type: 1, name: "Batalla de Boyacá" },
-      { date: "2015-12-08", type: 1, name: "Inmaculada Concepción" },
-      { date: "2015-12-25", type: 1, name: "Navidad" },
-      { date: "2015-01-12", type: 2, name: "Reyes Magos" },
-      { date: "2015-03-23", type: 2, name: "San José" },
-      { date: "2015-06-29", type: 2, name: "San Pedro y San Pablo" },
-      { date: "2015-08-17", type: 2, name: "Asunción de la Virgen" },
-      { date: "2015-10-12", type: 2, name: "Día de la Raza" },
-      { date: "2015-11-02", type: 2, name: "Todos los Santos" },
-      { date: "2015-11-16", type: 2, name: "Independencia de Cartagena" },
-      { date: "2015-04-02", type: 3, name: "Jueves Santo" },
-      { date: "2015-04-03", type: 3, name: "Viernes Santo" },
-      { date: "2015-05-18", type: 3, name: "Ascensión de Jesús" },
-      { date: "2015-06-08", type: 3, name: "Corpus Christi" },
-      { date: "2015-06-15", type: 3, name: "Sagrado Corazón de Jesús" }
+      { date: '2015-01-01', type: 1, name: 'Año Nuevo' },
+      { date: '2015-05-01', type: 1, name: 'Día del Trabajo' },
+      { date: '2015-07-20', type: 1, name: 'Grito de la Independencia' },
+      { date: '2015-08-07', type: 1, name: 'Batalla de Boyacá' },
+      { date: '2015-12-08', type: 1, name: 'Inmaculada Concepción' },
+      { date: '2015-12-25', type: 1, name: 'Navidad' },
+      { date: '2015-01-12', type: 2, name: 'Reyes Magos' },
+      { date: '2015-03-23', type: 2, name: 'San José' },
+      { date: '2015-06-29', type: 2, name: 'San Pedro y San Pablo' },
+      { date: '2015-08-17', type: 2, name: 'Asunción de la Virgen' },
+      { date: '2015-10-12', type: 2, name: 'Día de la Raza' },
+      { date: '2015-11-02', type: 2, name: 'Todos los Santos' },
+      { date: '2015-11-16', type: 2, name: 'Independencia de Cartagena' },
+      { date: '2015-04-02', type: 3, name: 'Jueves Santo' },
+      { date: '2015-04-03', type: 3, name: 'Viernes Santo' },
+      { date: '2015-05-18', type: 3, name: 'Ascensión de Jesús' },
+      { date: '2015-06-08', type: 3, name: 'Corpus Christi' },
+      { date: '2015-06-15', type: 3, name: 'Sagrado Corazón de Jesús' },
     ],
     2018: [
-      { date: "2018-01-01", type: 1, name: "Año Nuevo" },
-      { date: "2018-05-01", type: 1, name: "Día del Trabajo" },
-      { date: "2018-07-20", type: 1, name: "Grito de la Independencia" },
-      { date: "2018-08-07", type: 1, name: "Batalla de Boyacá" },
-      { date: "2018-12-08", type: 1, name: "Inmaculada Concepción" },
-      { date: "2018-12-25", type: 1, name: "Navidad" },
-      { date: "2018-01-08", type: 2, name: "Reyes Magos" },
-      { date: "2018-03-19", type: 2, name: "San José" },
-      { date: "2018-07-02", type: 2, name: "San Pedro y San Pablo" },
-      { date: "2018-08-20", type: 2, name: "Asunción de la Virgen" },
-      { date: "2018-10-15", type: 2, name: "Día de la Raza" },
-      { date: "2018-11-05", type: 2, name: "Todos los Santos" },
-      { date: "2018-11-12", type: 2, name: "Independencia de Cartagena" },
-      { date: "2018-03-29", type: 3, name: "Jueves Santo" },
-      { date: "2018-03-30", type: 3, name: "Viernes Santo" },
-      { date: "2018-05-14", type: 3, name: "Ascensión de Jesús" },
-      { date: "2018-06-04", type: 3, name: "Corpus Christi" },
-      { date: "2018-06-11", type: 3, name: "Sagrado Corazón de Jesús" }
-    ]
-  };
+      { date: '2018-01-01', type: 1, name: 'Año Nuevo' },
+      { date: '2018-05-01', type: 1, name: 'Día del Trabajo' },
+      { date: '2018-07-20', type: 1, name: 'Grito de la Independencia' },
+      { date: '2018-08-07', type: 1, name: 'Batalla de Boyacá' },
+      { date: '2018-12-08', type: 1, name: 'Inmaculada Concepción' },
+      { date: '2018-12-25', type: 1, name: 'Navidad' },
+      { date: '2018-01-08', type: 2, name: 'Reyes Magos' },
+      { date: '2018-03-19', type: 2, name: 'San José' },
+      { date: '2018-07-02', type: 2, name: 'San Pedro y San Pablo' },
+      { date: '2018-08-20', type: 2, name: 'Asunción de la Virgen' },
+      { date: '2018-10-15', type: 2, name: 'Día de la Raza' },
+      { date: '2018-11-05', type: 2, name: 'Todos los Santos' },
+      { date: '2018-11-12', type: 2, name: 'Independencia de Cartagena' },
+      { date: '2018-03-29', type: 3, name: 'Jueves Santo' },
+      { date: '2018-03-30', type: 3, name: 'Viernes Santo' },
+      { date: '2018-05-14', type: 3, name: 'Ascensión de Jesús' },
+      { date: '2018-06-04', type: 3, name: 'Corpus Christi' },
+      { date: '2018-06-11', type: 3, name: 'Sagrado Corazón de Jesús' },
+    ],
+  }
   Object.keys(holidaysYears).forEach(holidaysYear => {
     it(`Should return holidays for ${holidaysYear}`, () => {
-      expect(getAllHolidays(holidaysYear)).toEqual(holidaysYears[holidaysYear]);
-    });
-  });
+      expect(getAllHolidays(holidaysYear)).toEqual(holidaysYears[holidaysYear])
+    })
+  })
   it(`Should return holidays for the current year when no year given`, () => {
-    const currYear = new Date().getFullYear();
-    const currHols = getAllHolidays();
-    expect(currHols).toEqual(getAllHolidays(currYear));
-    expect(Array.isArray(currHols)).toBe(true);
-    expect(currHols.length).toBe(18);
-  });
-});
-describe("Should return error for an invalid date", () => {
-  it("should return error for a date bellow year <= 1983", () => {
+    const currYear = new Date().getFullYear()
+    const currHols = getAllHolidays()
+    expect(currHols).toEqual(getAllHolidays(currYear))
+    expect(Array.isArray(currHols)).toBe(true)
+    expect(currHols.length).toBe(18)
+  })
+})
+describe('Should return error for an invalid date', () => {
+  it('should return error for a date bellow year <= 1983', () => {
     expect(() => {
-      getHoliday(new Date("1983-12-31"));
-    }).toThrow("Invalid year. Should be an integer > 1983");
-  });
-  it("should return error for an invalid", () => {
+      getHoliday(new Date('1983-12-31'))
+    }).toThrow('Invalid year. Should be an integer > 1983')
+  })
+  it('should return error for an invalid', () => {
     expect(() => {
-      getHoliday("1983-12-31");
-    }).toThrow("Invalid date.");
-  });
-});
-describe("Should return error for a non valid year", () => {
-  it("should return error for a non integer number <= 1983", () => {
+      getHoliday('1983-12-31')
+    }).toThrow('Invalid date.')
+  })
+})
+describe('Should return error for a non valid year', () => {
+  it('should return error for a non integer number <= 1983', () => {
     expect(() => {
-      getAllHolidays(1983);
-    }).toThrow("Invalid year. Should be an integer > 1983");
+      getAllHolidays(1983)
+    }).toThrow('Invalid year. Should be an integer > 1983')
     expect(() => {
-      getAllHolidays(19);
-    }).toThrow("Invalid year. Should be an integer > 1983");
+      getAllHolidays(19)
+    }).toThrow('Invalid year. Should be an integer > 1983')
     expect(() => {
-      getAllHolidays("");
-    }).toThrow("Invalid year. Should be an integer > 1983");
+      getAllHolidays('')
+    }).toThrow('Invalid year. Should be an integer > 1983')
     expect(() => {
-      getAllHolidays([]);
-    }).toThrow("Invalid year. Should be an integer > 1983");
+      getAllHolidays([])
+    }).toThrow('Invalid year. Should be an integer > 1983')
     expect(() => {
-      getAllHolidays("asdf");
-    }).toThrow("Invalid year. Should be an integer > 1983");
-  });
-});
+      getAllHolidays('asdf')
+    }).toThrow('Invalid year. Should be an integer > 1983')
+  })
+})
