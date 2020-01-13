7458349d8a36b200e9c88991cf08a8ca9a273fed
Mauricio Robayo <me@mauriciorobayo.com>
2020-01-13 17:26:00 -0500

    chore: move lint-staged config to package

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "src/**/*.js": ["eslint --fix", "git add", "jest --bail --findRelatedTests"],
-  "**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}": [
-    "prettier --write",
-    "git add"
-  ]
-};
