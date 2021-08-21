8e9c8759f5978cd5983478dcfccc0217839ada45
Mauricio Robayo <rfmajo@gmail.com>
2021-08-21 17:41:52 -0500

    refactor: use function declaration
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import getHoliday from './helpers';
-import holidays from './holidays';
-import { ColombianHoliday } from './types';
+import getHoliday from "./helpers";
+import holidays from "./holidays";
+import { ColombianHoliday } from "./types";
 
 // 1984 is the year when the current holidays scheme is enforced
 // http://www.alcaldiabogota.gov.co/sisjur/normas/Norma1.jsp?i=4954
@@ -9,18 +9,18 @@ export const FIRST_HOLIDAY_YEAR = 1984;
 // The pascua package calculates Easter until 4099.
 export const LAST_HOLIDAY_YEAR = 4099;
 
-const colombianHolidays = (
-  year: number = new Date().getFullYear(),
-): ColombianHoliday[] => {
+function colombianHolidays(
+  year: number = new Date().getFullYear()
+): ColombianHoliday[] {
   if (year < FIRST_HOLIDAY_YEAR || year > LAST_HOLIDAY_YEAR) {
     throw new Error(
-      `The year should be between ${FIRST_HOLIDAY_YEAR} and ${LAST_HOLIDAY_YEAR}`,
+      `The year should be between ${FIRST_HOLIDAY_YEAR} and ${LAST_HOLIDAY_YEAR}`
     );
   }
 
   return holidays
     .map((holiday) => getHoliday(holiday, year))
     .sort((a, b) => a.celebrationDate.localeCompare(b.celebrationDate));
-};
+}
 
 export default colombianHolidays;
