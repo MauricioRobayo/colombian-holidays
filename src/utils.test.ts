499c9c1479c4d20b94e664a9725795042be93206
Mauricio Robayo <rfmajo@gmail.com>
2023-02-09 09:28:50 -0500

    feat(#477): add isHoliday function

---
+++ src/utils.test.ts
@@ -0,0 +1,10 @@
+import { isHoliday } from "./utils";
+
+describe("test isHoliday", () => {
+  it(`should return true for a holiday date object`, () => {
+    expect(isHoliday(new Date("2018-01-01T05:00:00.000Z"))).toBe(true);
+  });
+  it(`should return false for a non holiday date object`, () => {
+    expect(isHoliday(new Date("2018-01-02T05:00:00.000Z"))).toBe(false);
+  });
+});
