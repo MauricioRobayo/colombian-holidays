361a1f84f673b42a809c91f3b99acbb4560e703e
Mauricio Robayo <rfmajo@gmail.com>
2023-03-10 09:45:39 -0500

    refactor(#499): use valueAsDate for isHoliday

--- src/utils/isHoliday.ts
+++ src/utils/isHoliday.ts
@@ -1,14 +1,14 @@
 import colombianHolidays from "..";
 
 export function isHoliday(date: Date): boolean {
-  return colombianHolidays({ year: date.getUTCFullYear() }).some(
-    ({ celebrationDate }) => {
-      const d = new Date(celebrationDate);
-      return (
-        d.getUTCDate() === date.getUTCDate() &&
-        d.getUTCMonth() === date.getUTCMonth() &&
-        d.getUTCFullYear() === date.getUTCFullYear()
-      );
-    }
-  );
+  return colombianHolidays({
+    year: date.getUTCFullYear(),
+    valueAsDate: true,
+  }).some(({ celebrationDate }) => {
+    return (
+      celebrationDate.getUTCDate() === date.getUTCDate() &&
+      celebrationDate.getUTCMonth() === date.getUTCMonth() &&
+      celebrationDate.getUTCFullYear() === date.getUTCFullYear()
+    );
+  });
 }
