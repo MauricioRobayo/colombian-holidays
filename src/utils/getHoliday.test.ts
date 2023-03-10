33169eecc54803dea9ba15b15385a4ad801721f6
Mauricio Robayo <rfmajo@gmail.com>
2023-03-10 10:21:23 -0500

    test(#499): add timezones checks

--- src/utils/getHoliday.test.ts
+++ src/utils/getHoliday.test.ts
@@ -1,28 +1,46 @@
+import timezone_mock, { TimeZone } from "timezone-mock";
 import { getHoliday } from "./getHoliday";
 
-describe("test getHoliday", () => {
-  it(`should return holiday for a holiday date object`, () => {
-    const expectedHoliday = {
-      celebrationDate: "2018-01-01",
-      date: "2018-01-01",
-      name: { en: "New Year's Day", es: "Año Nuevo" },
-      nextMonday: false,
-    };
-    const holiday = getHoliday(new Date("2018-01-01T05:00:00.000Z"));
-    expect(holiday).toEqual(expectedHoliday);
-  });
+const timezones: TimeZone[] = [
+  "US/Pacific",
+  "US/Eastern",
+  "Brazil/East",
+  "UTC",
+  "Europe/London",
+  "Australia/Adelaide",
+];
 
-  it(`should return holiday with values as dates for a holiday date object`, () => {
-    const expectedHoliday = {
-      celebrationDate: new Date("2018-01-01T00:00:00.000Z"),
-      date: new Date("2018-01-01T00:00:00.000Z"),
-      name: { en: "New Year's Day", es: "Año Nuevo" },
-      nextMonday: false,
-    };
-    const holiday = getHoliday(new Date("2018-01-01T05:00:00.000Z"), {
-      valueAsDate: true,
+afterEach(() => {
+  timezone_mock.unregister();
+});
+
+describe("test getHoliday", () => {
+  describe.each(timezones)("%p", (timezone) => {
+    timezone_mock.register(timezone);
+
+    it(`should return holiday for a holiday date object`, () => {
+      const expectedHoliday = {
+        celebrationDate: "2018-01-01",
+        date: "2018-01-01",
+        name: { en: "New Year's Day", es: "Año Nuevo" },
+        nextMonday: false,
+      };
+      const holiday = getHoliday(new Date("2018-01-01"));
+      expect(holiday).toEqual(expectedHoliday);
     });
-    expect(holiday).toEqual(expectedHoliday);
+
+    it(`should return holiday with values as dates for a holiday date object`, () => {
+      const expectedHoliday = {
+        celebrationDate: new Date("2018-01-01T00:00:00.000Z"),
+        date: new Date("2018-01-01T00:00:00.000Z"),
+        name: { en: "New Year's Day", es: "Año Nuevo" },
+        nextMonday: false,
+      };
+      const holiday = getHoliday(new Date("2018-01-01"), {
+        valueAsDate: true,
+      });
+      expect(holiday).toEqual(expectedHoliday);
+    });
   });
 
   it(`should return false for a non holiday date object`, () => {
--- src/utils/isHoliday.test.ts
+++ src/utils/isHoliday.test.ts
@@ -1,10 +1,29 @@
+import timezone_mock, { TimeZone } from "timezone-mock";
 import { isHoliday } from "./isHoliday";
 
+const timezones: TimeZone[] = [
+  "US/Pacific",
+  "US/Eastern",
+  "Brazil/East",
+  "UTC",
+  "Europe/London",
+  "Australia/Adelaide",
+];
+
+afterEach(() => {
+  timezone_mock.unregister();
+});
+
 describe("test isHoliday", () => {
-  it(`should return true for a holiday date object`, () => {
-    expect(isHoliday(new Date("2018-01-01T05:00:00.000Z"))).toBe(true);
-  });
-  it(`should return false for a non holiday date object`, () => {
-    expect(isHoliday(new Date("2018-01-02T05:00:00.000Z"))).toBe(false);
+  describe.each(timezones)("%p", (timezone) => {
+    timezone_mock.register(timezone);
+
+    it(`should return true for a holiday date object`, () => {
+      expect(isHoliday(new Date("2018-01-01"))).toBe(true);
+    });
+
+    it(`should return false for a non holiday date object`, () => {
+      expect(isHoliday(new Date("2018-01-02"))).toBe(false);
+    });
   });
 });
