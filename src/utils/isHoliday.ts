d7ae226bef7e516af736969090e8d7294937afa5
Mauricio Robayo <rfmajo@gmail.com>
2025-05-17 23:29:38 -0500

    feat: memoize isHoliday

--- src/utils/isHoliday.ts
+++ src/utils/isHoliday.ts
@@ -1,9 +1,23 @@
 import colombianHolidays from "..";
+import type { ColombianHolidayWithNativeDate } from "../types";
 import { isSameDate } from "./helpers";
 
+const holidaysCache = new Map<number, ColombianHolidayWithNativeDate[]>();
+
 export function isHoliday(date: Date): boolean {
-  return colombianHolidays({
-    year: date.getUTCFullYear(),
+  const year = date.getUTCFullYear();
+  const cachedHolidays = holidaysCache.get(year);
+  if (cachedHolidays) {
+    return cachedHolidays.some(({ celebrationDate }) =>
+      isSameDate(celebrationDate, date)
+    );
+  }
+  const holidays = colombianHolidays({
+    year,
     valueAsDate: true,
-  }).some(({ celebrationDate }) => isSameDate(celebrationDate, date));
+  });
+  holidaysCache.set(year, holidays);
+  return holidays.some(({ celebrationDate }) =>
+    isSameDate(celebrationDate, date)
+  );
 }
